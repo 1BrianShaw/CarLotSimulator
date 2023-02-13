92242de75f73d5ce2b3ef1e36e61f7ee8e53c630
Brian Shaw <1BrianShaw@gmail.com>
2023-02-13 16:57:31 -0500

    exercise 2

--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -28,6 +28,51 @@ namespace CarLotSimulator
             //It should have at least one property: a List of cars
             //Instanciate the a Carlot at the beginning of the program and as you create a car add the car to the list.
             //At the end iterate through the list printing each of car's Year, Make, and Model to the console
+
+            //Standard Member Initialization
+            CarLot lot = new CarLot();
+            
+            Car bmw = new Car();
+            lot.Carlist.Add(bmw);
+
+            bmw.Year = 2018;
+            bmw.Make = "BMW";
+            bmw.Model = "328i";
+            bmw.EngineNoise = "Vroom";
+            bmw.HonkNoise = "Beep Beep";
+            bmw.IsDriveable= true;
+
+            bmw.MakeEngineNoise();
+            bmw.MakeHonkNoise();
+
+            //Object initializer syntax
+            Car celica = new Car() { Year = 1996, Make = "Toyota", Model = "Celica", EngineNoise = "putter", HonkNoise = "HAWNK", IsDriveable= true };
+            lot.Carlist.Add(celica);
+
+            celica.MakeEngineNoise();
+            celica.MakeHonkNoise();
+
+            //Constructor initialization
+            Car sonata = new Car(2022, "Hyundai", "Sonata", "vroom vroom", "beep beep", true);
+            lot.Carlist.Add(sonata);
+
+            sonata.MakeEngineNoise();
+            sonata.MakeHonkNoise();
+
+            Console.WriteLine("-------------");
+
+            foreach(var car in lot.Carlist)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Year:{car.Year} Make: {car.Make} Model: {car.Model}");
+            }
+
+
+
+
+
+
+
         }
     }
 }
